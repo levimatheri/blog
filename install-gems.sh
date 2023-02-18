80eaa79ff7d2d920d085931b8d4adc66b9ea81c8
Levi <levimatheri@gmail.com>
2023-02-18 16:12:12 -0500

    Add APIM policy parameterization post

--- install-gems.sh
+++ install-gems.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-rm -f Gemfile
-rm -f Gemfile.lock
-bundle init
-touch Gemfile.lock
-chmod a+w Gemfile.lock
-chmod 777 Gemfile
-bundle add webrick
-bundle add jekyll-watch
-bundle add kramdown-parser-gfm
-bundle add kramdown rouge
+rm -f Gemfile &&
+rm -f Gemfile.lock &&
+bundle init &&
+touch Gemfile.lock &&
+chmod a+w Gemfile.lock &&
+chmod 777 Gemfile &&
+bundle add webrick &&
+bundle add jekyll &&
+bundle add jekyll-watch &&
+bundle add kramdown-parser-gfm &&
+bundle add kramdown rouge &&
+bundle install
